a53d2ff61880b5992e49670fca695b7a955f7f38
Nathan Ferreira Borges <borges3141@gmail.com>
2025-07-21 22:17:33 -0300

    tentando criar uma porta http para o bot

--- index.js
+++ index.js
@@ -182,6 +182,17 @@ async function handleIncomingMessage(message) {
 	startSessionTimeout(from, message);
 }
 
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('Bot is running!'));
+
+app.listen(PORT, '0.0.0.0', () => {
+	console.log(`HTTP server listening on port ${PORT}`);
+});
+
 client.on('qr', (qr) => qrcode.generate(qr, { small: true }));
 
 client.on('ready', () => console.log('Cliente está pronto'));
